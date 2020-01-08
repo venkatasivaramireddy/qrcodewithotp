0ff2069d5a4b0c1b4b688b3e54d1818624c667aa
venkatasivaramireddy <40483818+venkatasivaramireddy@users.noreply.github.com>
2020-01-08 18:18:21 +0530

    hello

--- qrcodewithotp/app/static/one.py
+++ qrcodewithotp/app/static/one.py
@@ -1,14 +1,16 @@
-n=input()
-lista=[]
-for i in range(int(n)):
-    lista.append(input())
+# n=input()
+# # # lista=[]
+# # # for i in range(int(n)):
+# # #     lista.append(input())
+# # #
+# # # dictionary={}
+# # #
+# # # for i in range(int(n)):
+# # #     if lista[i] in dictionary:
+# # #         dictionary[lista[i]]=dictionary.get(lista[i])+1
+# # #     else:
+# # #         dictionary[lista[i]]=1
+# # # for w in sorted(dictionary, key=dictionary.get, reverse=True):
+# # #       print (w)
 
-dictionary={}
-
-for i in range(int(n)):
-    if lista[i] in dictionary:
-        dictionary[lista[i]]=dictionary.get(lista[i])+1
-    else:
-        dictionary[lista[i]]=1
-for w in sorted(dictionary, key=dictionary.get, reverse=True):
-      print (w)
+print("hello")
